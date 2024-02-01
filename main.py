22170aeed1e6dd96d4ecc304bcfa7ee281e9f422
روبونامریی <107060501+robonamari@users.noreply.github.com>
2024-02-01 19:55:09 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+import random
+
+links_response = requests.get("https://raw.githubusercontent.com/robonamari/meme-api/main/URL.json")
+links_data = links_response.json()["links"]
+random_link = random.choice(links_data)
+
+print(f"{random_link}")
