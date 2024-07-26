ec76566acc4a789f5090be0e101c68f2b4acf0d0
robonamari <107060501+robonamari@users.noreply.github.com>
2024-07-26 10:26:42 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 import requests
 import random
 
-links_response = requests.get('https://raw.githubusercontent.com/robonamari/meme-api/main/URL.json')
-links_data = links_response.json()['links']
-random_link = random.choice(links_data)
-
-print(random_link)
+print(random.choice(requests.get('https://raw.githubusercontent.com/robonamari/meme-api/main/URL.json').json()['links']))
