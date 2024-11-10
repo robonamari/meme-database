9b89b2270042d1d1797503910c20ea398bf9d5b2
robonamari <ROBONAMARI@GMAIL.COM>
2024-11-10 14:07:17 +0330

    تغییر لینک کد مثال

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import requests
 import random
 
-print(random.choice(requests.get('https://raw.githubusercontent.com/robonamari/meme-database/main/URL.json').json()['links']))
+print(random.choice(requests.get('https://meme-database.robonamari.com/URL.json').json()['links']))
